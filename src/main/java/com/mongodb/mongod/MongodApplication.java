58a248fe59f465cccc68111eac3d501a143e645d
Draeter <luanfel2009@gmail.com>
2020-11-24 16:55:09 -0300

    Ambientação de projeto

---
+++ src/main/java/com/mongodb/mongod/MongodApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.mongod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongodb/mongod/MongodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.mongod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
