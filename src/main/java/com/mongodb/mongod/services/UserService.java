0ff208313836b4682175d0fcbc39db6717136647
Draeter <luanfel2009@gmail.com>
2020-12-08 18:11:11 -0300

    Implementação de pesquisa de usuario por id e tratamento de exceçào

--- src/main/java/com/mongodb/mongod/services/UserService.java
+++ src/main/java/com/mongodb/mongod/services/UserService.java
@@ -2,10 +2,12 @@ package com.mongodb.mongod.services;
 
 import com.mongodb.mongod.domain.User;
 import com.mongodb.mongod.repository.UserRepository;
+import com.mongodb.mongod.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,7 +15,13 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
+
