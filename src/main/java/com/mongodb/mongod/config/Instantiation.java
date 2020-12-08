4ff5077478015f526861e44e6328f3d9b6fc849e
Draeter <luanfel2009@gmail.com>
2020-12-08 16:31:16 -0300

    Instanciação da base de dados;

---
+++ src/main/java/com/mongodb/mongod/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.mongodb.mongod.config;
+
+import com.mongodb.mongod.domain.User;
+import com.mongodb.mongod.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "bob@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
