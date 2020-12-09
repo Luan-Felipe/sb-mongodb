afa2c60324a2ba2eb5fb24f1d5c0b58f3dd6a902
Draeter <luanfel2009@gmail.com>
2020-12-09 15:55:08 -0300

    Obtendo posts por id

---
+++ src/main/java/com/mongodb/mongod/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mongodb.mongod.services;
+
+import com.mongodb.mongod.domain.Post;
+import com.mongodb.mongod.domain.User;
+import com.mongodb.mongod.repository.PostRepository;
+import com.mongodb.mongod.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    public PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
