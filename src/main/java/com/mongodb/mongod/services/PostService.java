96ceac88403c366ff3bc09d73874afb00d8b63fc
Draeter <luanfel2009@gmail.com>
2020-12-09 17:39:59 -0300

    Implementacão de consulta com varios critérios

--- src/main/java/com/mongodb/mongod/services/PostService.java
+++ src/main/java/com/mongodb/mongod/services/PostService.java
@@ -1,12 +1,12 @@
 package com.mongodb.mongod.services;
 
 import com.mongodb.mongod.domain.Post;
-import com.mongodb.mongod.domain.User;
 import com.mongodb.mongod.repository.PostRepository;
 import com.mongodb.mongod.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +23,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public  List<Post> completeSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.completeSearch(text, minDate, maxDate);
     }
 }
