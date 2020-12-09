f742d1ea21a672f8024c31a247b0f3d2fe02639f
Draeter <luanfel2009@gmail.com>
2020-12-09 13:48:16 -0300

    Referenciando os posts do usuario

--- src/main/java/com/mongodb/mongod/config/Instantiation.java
+++ src/main/java/com/mongodb/mongod/config/Instantiation.java
@@ -38,8 +38,11 @@ public class Instantiation implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para o Rio de Janeiro. Abraços!", new AuthorDTO(maria));
-        Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
